3efdbc6ccb7c2e4154b2331096f5029232c29f9a
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-08-30 15:04:04 -0400

    add some html and maybe connection to grid engine

---
+++ server/config/connection.js
@@ -0,0 +1,19 @@
+const gameConfig = {
+
+  
+    plugins: {
+      scene: [
+        {
+          key: "gridEngine",
+          plugin: GridEngine,
+          mapping: "gridEngine",
+        },
+      ],
+    },
+  
+
+  };
+  
+  const game = new Phaser.Game(gameConfig);
+
+  module.exports = game;
