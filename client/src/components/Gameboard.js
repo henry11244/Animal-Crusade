c4bf0443b27442577c81bb5ec8a836f904cf0319
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-08-31 11:16:23 -0700

    added canvas tags in gameboard component

--- client/src/components/Gameboard.js
+++ client/src/components/Gameboard.js
@@ -1,11 +1,24 @@
-import React from 'react';
-
+import React from "react";
 
 const Gameboard = () => {
-
-    return (
-        <div> gameboard </div>
-    );
+  return (
+    <div>
+      {" "}
+      <canvas
+        width="720"
+        height="528"
+        style={{
+          imageRendering: "pixelated",
+          marginLeft: "8px",
+          marginTop: "6px",
+        }}
+      ></canvas>
+      gameboard
+      <script src="phaser.min.js"></script>
+      <script src="GridEngine.min.js"></script>
+      <script src="gameConfig.js"></script>
+    </div>
+  );
 };
 
 export default Gameboard;
