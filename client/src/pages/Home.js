3f50cae478a9ac483f2e827a1086226ed070af13
Henry Nguyen <hln11244@gmail.com>
2022-08-30 18:00:01 -0700

    added home page

---
+++ client/src/pages/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+
+import Gameboard from '../components/Gameboard';
+import Scoreboard from '../components/Scoreboard';
+
+import { QUERY_PROFILES } from '../utils/queries';
+
+const Home = () => {
+    const { loading, data } = useQuery(QUERY_PROFILES);
+    const profiles = data?.profiles || [];
+
+    return (
+        <main>
+            <div className="flex-row justify-center">
+                <div className="col-12 col-md-10 my-3">
+                    {loading ? (
+                        <div>Loading...</div>
+                    ) : (
+                        <ProfileList
+                            profiles={profiles}
+                            title="Here's the current roster of friends..."
+                        />
+                    )}
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default Home;
