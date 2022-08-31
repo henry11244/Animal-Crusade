cb4a595e9ba6ed40ba2e1c29cf0aa64f0b970c79
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-08-31 14:15:14 -0700

    reinstalled node modules

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,27 +1,27 @@
 import React from "react";
-import { useQuery } from "@apollo/client";
+// import { useQuery } from "@apollo/client";
 
 import Gameboard from "../components/Gameboard";
 import Scoreboard from "../components/Scoreboard";
 
-import { QUERY_USER } from "../utils/queries";
+// import { QUERY_USER } from "../utils/queries";
 
 const Home = () => {
-  const { loading, data } = useQuery(QUERY_USER);
-  console.log(data);
+  // const { loading, data } = useQuery(QUERY_USER);
+  // console.log(data);
 
   return (
     <main>
       <div className="flex-row justify-center">
         <div className="col-12 col-md-10 my-3">
-          {loading ? (
+          {/* {loading ? (
             <div>Loading...</div>
-          ) : (
+          ) : ( */}
             <>
               <Gameboard />
               <Scoreboard />
             </>
-          )}
+          {/* )} */}
         </div>
       </div>
     </main>
