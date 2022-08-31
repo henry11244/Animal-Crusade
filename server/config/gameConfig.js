88d4cf42694290dbfa473c930d159ecde4dd51bf
Henry Nguyen <hln11244@gmail.com>
2022-08-30 17:29:19 -0700

    added password validator

--- server/config/gameConfig.js
+++ server/config/gameConfig.js
@@ -1,23 +1,23 @@
 const game = new Phaser.Game(config);
 
 function preload() {
-    this.load.tilemapTiledJSON("game-map", "map/animalcrusademap.json");
-  }
+  this.load.tilemapTiledJSON("game-map", "map/animalcrusademap.json");
+}
 
 function create() {
 
-  
-    const gridEngineConfig = {
-      characters: [
-        {
-          id: "player",
-          sprite: playerSprite,
-          walkingAnimationMapping: 6,
-        },
-      ],
-    };
-  
-    this.gridEngine.create(tilemap, gridEngineConfig);
-  
 
-  }
+  const gridEngineConfig = {
+    characters: [
+      {
+        id: "player",
+        sprite: playerSprite,
+        walkingAnimationMapping: 6,
+      },
+    ],
+  };
+
+  this.gridEngine.create(tilemap, gridEngineConfig);
+
+
+}
