cf50ecd15cdb6ba76c8719f9763a2fad0b7ea47d
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-08-31 13:49:04 -0700

    removed grid engine files

--- client/src/components/Gameboard.js
+++ client/src/components/Gameboard.js
@@ -3,19 +3,7 @@ import React from "react";
 const Gameboard = () => {
   return (
     <div>
-      {" "}
-      <canvas
-        width="720"
-        height="528"
-        style={{
-          imageRendering: "pixelated",
-          marginLeft: "8px",
-          marginTop: "6px",
-        }}
-      ></canvas>
       gameboard
-
-
     </div>
   );
 };
