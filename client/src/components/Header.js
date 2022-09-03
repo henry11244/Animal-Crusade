77764cdc6f41953b560b993f6dea82e71d8a1b14
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-09-03 14:49:22 -0700

    removed extra spaces for formatting

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,10 +10,7 @@ const Header = () => {
   };
   return (
     <header className="bg-info text-dark mb-4 py-3 display-flex align-center">
-
-
       <div className="container flex-column justify-space-between-lg justify-center align-center text-center">
-
         <Link className="text-dark" to="/">
           <h1 className="m-0" style={{ fontSize: "3rem" }}>
             Animal Crusade
